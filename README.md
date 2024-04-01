04b0f4cc558f3b44ecf17d4032a62f9627f578d9
Dommie-Blessed <mangesaoigoro@gmail.com>
2024-04-02 00:02:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-tested-app
---
+++ tested.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def main():
+    st.title('Test Data Science App')
+    st.write('Welcome to my new app')
+    st.text('This is a simple test for the project')
